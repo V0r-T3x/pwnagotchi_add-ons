f31c8dd895c707987aba42b66cbf92af78cc4cfb
V0rT3x <70115207+V0r-T3x@users.noreply.github.com>
2024-03-20 18:29:03 -0400

    Update Archivist.py

--- Archivist.py
+++ Archivist.py
@@ -9,10 +9,19 @@ repositories = [
     "https://github.com/allordacia/Pwnagotchi-Handshaker",
 ]
 
-def clone_or_update_repository(repo_url, author, repo_name):
-    print(author)
-    print(repo_name)
-    print(repo_url)
+def clone_or_update_repository(repo_url):
+    # Split the URL to extract the repository owner and name
+    parts = repo_url.strip("/").split("/")
+    if len(parts) == 5:  # If the URL contains both owner and repo name
+        author = parts[-2]
+        repo_name = parts[-1]
+    else:  # If the URL contains only the repo name
+        author = ""
+        repo_name = parts[-1]
+
+    print("Author:", author)
+    print("Repository:", repo_name)
+    print("URL:", repo_url)
     #author_directory = os.path.join(author, repo_name)
     #if not os.path.exists(author_directory):
         #print(author_directory)
