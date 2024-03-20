f575cae953cfa1a783b88c7b4abdc02e0c494389
V0rT3x <70115207+V0r-T3x@users.noreply.github.com>
2024-03-20 18:55:04 -0400

    Update Archivist.py

--- Archivist.py
+++ Archivist.py
@@ -34,11 +34,10 @@ def clone_or_update_repository(repo_url):
 def main():
     # Clone or update repositories
     print("Hack The Planet")
-    print(os.path)
+    #print(os.path)
     for repo_url in repositories:
         #author, repo_name = repo_url.split("/")[-2:]
         clone_or_update_repository(repo_url)
-    print(os.getcwd())
     # Commit and push changes
     #subprocess.run(["git", "config", "--local", "user.email", "action@github.com"])
     #subprocess.run(["git", "config", "--local", "user.name", "GitHub Action"])
