d25ef32f2ed605c236c89a94778dd76bb1966a1d
V0rT3x <70115207+V0r-T3x@users.noreply.github.com>
2024-03-19 20:29:36 -0400

    Create clone_script.py

---
+++ .github/workflows/clone_script.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+# List of repositories to include in the archive
+repositories = [
+
+    "https://github.com/rai68/gpsd-easy",
+    "https://github.com/rai68/pwnagotchi-pisugar2-plugin/",
+    "https://github.com/allordacia/FlipperLink",
+    "https://github.com/allordacia/Pwnagotchi-Handshaker",
+]
+
+# Directory where the plugins_archive repository is cloned
+archive_directory = "plugins_archive"
+
+def clone_or_update_repository(repo_url, author):
+    author_directory = os.path.join(archive_directory, author)
+    if not os.path.exists(author_directory):
+        os.makedirs(author_directory)
+        subprocess.run(["git", "clone", repo_url, author_directory])
+    else:
+        os.chdir(author_directory)
+        subprocess.run(["git", "pull"])
+
+def main():
+    # Clone or update repositories
+    for repo_url in repositories:
+        author = repo_url.split("/")[-2]
+        clone_or_update_repository(repo_url, author)
+
+    # Commit and push changes
+    os.chdir(archive_directory)
+    subprocess.run(["git", "config", "--local", "user.email", "action@github.com"])
+    subprocess.run(["git", "config", "--local", "user.name", "GitHub Action"])
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "Update archive"])
+    subprocess.run(["git", "push"])
+
+if __name__ == "__main__":
+    main()
